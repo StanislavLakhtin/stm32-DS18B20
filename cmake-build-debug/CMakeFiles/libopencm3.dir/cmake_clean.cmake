fffa89487844a0c6743f6104be2358769e6780f2
Stanislav Lakhtin <savant@savant.pro>
2017-01-30 17:59:16 +0300

    fixed usart to 1wire

--- cmake-build-debug/CMakeFiles/libopencm3.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/libopencm3"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/libopencm3.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
